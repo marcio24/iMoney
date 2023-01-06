b59c6ce89359461b728eb5a4e60605426c604b05
Márcio Filho <79327001+marcio24@users.noreply.github.com>
2023-01-06 15:19:54 -0400

    iMoney v1.0

---
+++ script.js
@@ -0,0 +1,6 @@
+function reloadIF() {
+    var iframe = document.getElementById('iframe');
+iframe.src = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSHDnaiJyPMqJAVfjoGyzIl6UofQdX1UEUVVyYa1SuqFkzCLdsOOHjAwAEBLzwN3iTx3sjya5aBFOEZ/pubhtml?gid=1850553581&amp;single=true&amp;widget=false&amp;headers=false";
+console.log(iframe.src);
+    
+}
